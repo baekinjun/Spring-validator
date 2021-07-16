b12720dc83de656d675bd59bc1a133a4387ce217
baekinjun <binjun0711@gmail.com>
2021-07-16 14:33:26 +0900

    first commit

---
+++ src/main/java/com/example/validation/dto/Car.java
@@ -0,0 +1,51 @@
+package com.example.validation.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import javax.validation.constraints.NotBlank;
+
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class Car {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String carNumber;
+
+    @NotBlank
+    private String type;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", carNumber='" + carNumber + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/validation/dto/User.java
@@ -0,0 +1,91 @@
+package com.example.validation.dto;
+
+import com.example.validation.annotation.YearMonth;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class User {
+
+    @NotBlank
+    private String name;
+
+    @Max(value = 90)
+    private int age;
+
+    @YearMonth
+    private String reqYearMonth;
+
+    @Valid
+    private List<Car> cars;
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    @Email
+    private String email;
+
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호가 맞지 않습니다.")
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getReqYearMonth() {
+        return reqYearMonth;
+    }
+
+    public void setReqYearMonth(String reqYearMonth) {
+        this.reqYearMonth = reqYearMonth;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", reqYearMonth='" + reqYearMonth + '\'' +
+                ", cars=" + cars +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
